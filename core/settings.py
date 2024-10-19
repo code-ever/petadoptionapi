8716ef22bccf8a165f1d09c464ed98f78413a018
code-ever <iykeiykehillary@gmail.com>
2024-10-19 22:49:21 +0100

    add url to origin

--- core/settings.py
+++ core/settings.py
@@ -138,11 +138,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:9000",
-    "https://petadoptionapi.onrender.com",
-]
+CORS_ALLOW_ALL_ORIGINS=True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
